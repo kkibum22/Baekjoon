b2dc459e9b9a0ec1be4a7223505073f3b8a0790e
김기범 <136368145+kkibum22@users.noreply.github.com>
2023-12-27 10:17:09 +0900

    [Bronze V] Title: 사칙연산, Time: 128 ms, Memory: 9608 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+// "/dev/stdin"
+
+let a = parseInt(input[0]);
+let b = parseInt(input[1]);
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(parseInt(a / b));
+console.log(a % b);
