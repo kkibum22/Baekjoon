5bf1668c6a0fdb7b83b55dbd436eb603b194c112
김기범 <136368145+kkibum22@users.noreply.github.com>
2023-12-27 10:04:07 +0900

    [Bronze V] Title: A×B, Time: 124 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+// "/dev/stdin"
+
+let a = parseInt(input[0]);
+let b = parseInt(input[1]);
+
+console.log(a * b);
