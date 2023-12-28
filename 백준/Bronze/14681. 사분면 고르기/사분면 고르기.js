e3db3e1e64369c5cd68d778fe5732ebecd017242
김기범 <136368145+kkibum22@users.noreply.github.com>
2023-12-28 09:46:07 +0900

    [Bronze V] Title: 사분면 고르기, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.js
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.js
@@ -1,16 +1,21 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split("\n");
 
-let a = parseInt(input[0]);
-let b = parseInt(input[1]);
-// 
+// "/dev/stdin"
 
-if (a > 0 && b > 0) {
-  console.log(1);
-} else if (a < 0 && b > 0) {
-  console.log(2);
-} else if (a < 0 && b < 0) {
-  console.log(3);
-} else if (a > 0 && b < 0) {
-  console.log(4);
+let x = parseInt(input[0]);
+let y = parseInt(input[1]);
+
+let result = 0;
+
+if (x > 0 && y > 0) {
+  result = 1;
+} else if (x < 0 && y > 0) {
+  result = 2;
+} else if (x < 0 && y < 0) {
+  result = 3;
+} else {
+  result = 4;
 }
+
+console.log(result);
