1a70ef8d86aa16a6ea8df676c7c4e8ed34157b7f
김기범 <136368145+kkibum22@users.noreply.github.com>
2023-12-30 21:53:33 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 120 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.js
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.js
@@ -1,22 +1,16 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim();
-// 
+// "/dev/stdin"
 
-let N = parseInt(input);
+let answer = "";
+
+for (let i = 1; i <= input; i += 4) {
+  answer += "long ";
+}
+console.log(`${answer}int`);
 
 // N은 4의배수
 // 4의배수 1번 당 롱 1번
 
 // 4 > long int
 // 20 > long long long long int
-
-let answer = ""
-
-for (let i = 4; i <= N; i++) {
-
-  if (!(i % 4)) {
-    answer += "long"
-}
-}
-
-console.log(`${answer.split("g").join("g ")}int`)
