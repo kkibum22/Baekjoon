f10e2dca02aaef70575541eeb316f047e5d8ea6c
김기범 <136368145+kkibum22@users.noreply.github.com>
2023-12-30 21:23:02 +0900

    [Bronze IV] Title: 영수증, Time: 116 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.js
+++ 백준/Bronze/25304. 영수증/영수증.js
@@ -1,18 +1,20 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
-// 
+// "/dev/stdin"
 
-let X = parseInt(input[0]);
-let N = parseInt(input[1]);
+// 첫째 줄에는 영수증에 적힌 총 금액 X가 주어진다.
+// 둘째 줄에는 영수증에 적힌 구매한 물건의 종류의 수 N이 주어진다.
+// N개의 줄에는 각 물건의 가격 a와 개수 b가 공백을 사이에 두고 주어진다.
 
-const answer = [];
-let total = 0;
+let answer = 0;
 
-for (let i = 0; i < N; i++) {
-  const a = input[i + 2].split(" ").map((n) => Number(n));
-  let price = a[0] * a[1];
-  total += price;
+for (let i = 1; i <= input[1]; i++) {
+  let price = input[i + 1].split(" ");
+  answer += parseInt(price[0]) * parseInt(price[1]);
 }
 
-X === total ? console.log("Yes") : console.log("No");
-
+if (answer === parseInt(input[0])) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
