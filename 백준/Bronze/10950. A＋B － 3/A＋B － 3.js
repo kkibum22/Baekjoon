00f2df087f38af6650b878a7e986b80d8b8f901c
김기범 <136368145+kkibum22@users.noreply.github.com>
2023-12-30 21:05:19 +0900

    [Bronze V] Title: A+B - 3, Time: 132 ms, Memory: 9580 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
-// 
+// "/dev/stdin"
 
-let a = input[0];
+let num = parseInt(input[0]);
 
-for (let i = 1; i <= a; i++) {
+for (let i = 1; i <= num; i++) {
   let score = input[i].split(" ");
-  console.log(Number(score[0]) + Number(score[1]));
+  console.log(parseInt(score[0]) + parseInt(score[1]));
 }
