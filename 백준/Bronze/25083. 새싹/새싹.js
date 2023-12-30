1201b4a0f4194bb1a1716f86505ad51ec4958b03
김기범 <136368145+kkibum22@users.noreply.github.com>
2023-12-30 20:35:46 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+// "/dev/stdin"
+
+console.log(`         ,r'"7
+r\`-_   ,'  ,/
+ \\. ". L_r'
+   \`~\\/
+      |
+      |`);
